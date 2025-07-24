35243d2cbf1de11b43f720082432889f324433d8
ねるうさぎちゃん <fanraruku@gmail.com>
2025-07-25 01:27:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,27 +11,33 @@
     for (i = 2025; i >= 1900; --i) {
       let op = document.createElement("option");
       op.value = i;
-      op.text = i;
-      select_year.appendChild(op);
-
-      
+      //op.text = i;
       // 元号を追加
-      let label = `${year}`;
-      if (year === 2019){ 
-        label += "令和 元";
+      let label = `${i}`;
+      if (i === 2019){ 
+        label += " (令和元) ";
       }
-      else if(year > 2019){
-        label += `令和${year - 2018}`
+      else if(i > 2019){
+        label += ` (令和${i - 2018}) `
       }
+      else if (i === 1989){
+
+      }
+      op.textContent = label;
       select_year.appendChild(op);
     }
   }
   function $set_month() {
     //月_生成
     for (i = 1; i <= 12; ++i) {
+
+      // <option></option>をつくる
       let op = document.createElement("option");
+      // ユーザーがこの選択肢を選ぶと、送信されるもの
       op.value = i;
+      // ユーザーに見える部分
       op.text = i;
+      // select_monthに追加
       select_month.appendChild(op);
     }
   }
