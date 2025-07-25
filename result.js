ab7d94ae56fb5340b61f8ba776dc129dd29cf2e4
Irodorinoiro <fanraruku@gmail.com>
2025-07-25 17:31:51 +0900

    遷移ページ制作

--- result.js
+++ result.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContantLoaded", function()){
     const params = new URLSearchParams(window.location.search);
     // パラメータからnameを取得
     const name = params.get("name");
-}
+};
 
 // URLで遷移するときは, URLにパラメータをつけて遷移する方法しかない
 // そのためparamsを使ってURLのパラメータを取得する
