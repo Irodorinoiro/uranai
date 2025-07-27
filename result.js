783dc6ea19198f353f1c59bbfe50423e6dd1cb41
Irodorinoiro <fanraruku@gmail.com>
2025-07-27 14:35:41 +0900

    変数をグローバル変数に変更

--- result.js
+++ result.js
@@ -1,27 +1,33 @@
+let today = new Date();
+let today_month = today.getMonth() + 1;
+let today_day = today.getDate();
+let today_info = today.getFullYear() + today_month + today_day;
+
+// URLのパラメータを取得する関数
+const params = new URLSearchParams(window.location.search);
+// パラメータからnameを取得
+const name = params.get("name");
+const year = parseInt(params.get("year"));
+const month = parseInt(params.get("month"));
+const day = parseInt( params.get("day"));
+
 (function () {
   "use strict";
-  const info_output = document.getElementById("info_output");
+  const name_output = document.getElementById("name_output");
+  const title_output = document.getElementById("title_output");
 
-  function $info_output() {
-    // URLのパラメータを取得する関数
-    const params = new URLSearchParams(window.location.search);
-    // パラメータからnameを取得
-    const name = params.get("name");
-    const year = parseInt(params.get("year"));
-    const month = parseInt(params.get("month"));
-    const day = parseInt( params.get("day"));
-
-    let today = new Date();
-    let today_month = today.getMonth();
-    let today_day = today.getDate();
-    let today_info = today.getFullYear() + today_month + today_day;
+  function $name_output() {
     // HTMLで表示する
-    info_output.textContent = `${today_month}月${today_day}日の占い結果\n`;
-    info_output.textContent += `${name}さんの運勢\n`;
-    //info_output.textContent += `${year}年${month}月${day}日生まれ`;
+    name_output.textContent += `${name}さんの運勢\n`;
+    //name_output.textContent += `${year}年${month}月${day}日生まれ`;
+  }
+
+  function $title_output(){
+    title_output.textContent = `${today_month}月${today_day}日の占い結果\n`;
   }
   window.addEventListener("DOMContentLoaded", function () {
-    $info_output();
+    $name_output();
+    $title_output();
   });
 })();
 // URLで遷移するときは, URLにパラメータをつけて遷移する方法しかない
